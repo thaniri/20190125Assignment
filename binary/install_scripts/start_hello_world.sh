7143910260950999f69d2bfba2ce041e502d70a8
Marek Slabicki <thaniri@gmail.com>
2019-01-25 10:26:56 -0800

    CodeDeploy can't put shell commands into the background with &. Going to make a daemon instead.

--- binary/install_scripts/start_hello_world.sh
+++ binary/install_scripts/start_hello_world.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-/home/ubuntu/hello_world_app &
+cp /home/ubuntu/install_scripts/systemd_unit_file/hello_world.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl start hello_world.service
+systemctl status hello_world.service
+
